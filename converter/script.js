a2e353c1f77e8f1726b3a48bf7526806d39a02dc
kupeck <70723820+kupeck@users.noreply.github.com>
2020-12-16 21:24:41 +0100

    Exponential

--- converter/script.js
+++ converter/script.js
@@ -1,4 +1,16 @@
 function sqr() {
-    var i = document.getElementById("sqr").value;
-    document.getElementById("sqrout").value = i * i;
+    var sqr = document.getElementById("sqr").value;
+    document.getElementById("sqrout").value = sqr * sqr;
+}
+function cub() {
+    var cub = document.getElementById("cub").value;
+    document.getElementById("cubout").value = cub * cub * cub;
+}
+function hcu() {
+    var hcu = document.getElementById("hcu").value;
+    document.getElementById("hcuout").value = hcu * hcu * hcu * hcu;
+}
+function hhc() {
+    var hhc = document.getElementById("hhc").value;
+    document.getElementById("hhcout").value = hhc * hhc * hhc * hhc * hhc;
 }
