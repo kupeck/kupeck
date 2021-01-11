2e1294dc657f21a520c61e6ce278a3168983982c
kupeck <70723820+kupeck@users.noreply.github.com>
2021-01-11 18:49:33 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+i = 0
+n = 0
+while i < 26:
+    i = i + 1
+    if i > 25:
+        n = n + 1
+        i = 0
+    print(alphabet[n] + " " + alphabet[i])
+print ("Done!")
