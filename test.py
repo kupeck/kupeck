930aa0ebc07e4a91c2806aaa47bfaa799c270a43
kupeck <70723820+kupeck@users.noreply.github.com>
2021-01-12 09:39:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 i = 0
 n = 0
-while i < 26:
+while n < 25:
     i = i + 1
     if i > 25:
         n = n + 1
