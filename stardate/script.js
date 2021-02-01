ac521b48ebc99f20c511be948ecab47c02c88720
kupeck <70723820+kupeck@users.noreply.github.com>
2021-02-01 16:22:26 +0100

    Few updates

--- stardate/script.js
+++ stardate/script.js
@@ -5,6 +5,9 @@ function tosd() {
     var tstardate = ((tmiliunix - 11139552000000) / 1000) / 31536;
     console.log("Time: " + ttime + " miliUnix: " + tmiliunix + " Stardate: " + tstardate);
     document.getElementById("to-sd-sd").value = tstardate;
+    if (document.getElementById("to-sd-sd").value = "NaN") {
+        alert("You may have to specify AM/PM if that does not work, contact me at: kupeck#9974 on discord")
+    }
 }
 function fromsd() {
     var fstardate = document.getElementById('from-sd-sd').value;
