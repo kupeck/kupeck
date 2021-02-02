6a182fa9f0f7432d09ec8f944cb95fa63d7f8419
kupeck <70723820+kupeck@users.noreply.github.com>
2021-02-02 21:00:02 +0100

    wip 2

---
+++ calculator/script.js
@@ -0,0 +1,8 @@
+var input = document.getElementById('input');
+
+function addtoin(adding) {
+    input.value = input.value + adding;
+    console.log("Added " + adding + " to input.")
+}
+
+console.log("Loaded.")
