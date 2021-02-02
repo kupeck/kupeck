35d6a084e62ea95b10c43015154b3b6588b26d4b
kupeck <70723820+kupeck@users.noreply.github.com>
2021-02-02 11:48:39 +0100

    Update test2.py

--- py/test2.py
+++ py/test2.py
@@ -2,6 +2,7 @@ import requests
 i = 0
 while i < 50001:
     r = requests.get('https://ping.kupeck.workers.dev/')
-    print(r.text + " " + str(r.status_code) + " " + str(i))
+    if i % 50 == 0:
+        print(r.text + " " + str(r.status_code) + " " + str(i))
     i = i + 1
 print('Done!')
