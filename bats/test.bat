fe422171de02eb6804b0bded2558b2008d0b1509
kupeck <70723820+kupeck@users.noreply.github.com>
2021-02-07 16:23:25 +0100

    Update test.bat

--- bats/test.bat
+++ bats/test.bat
@@ -1,2 +1,17 @@
-timeout /T 600
+timeout /T 300
+:loop
 taskkill /T /IM opera* /F
+timeout /T 60
+taskkill /T /IM opera* /F
+timeout /T 30
+taskkill /T /IM opera* /F
+timeout /T 40
+taskkill /T /IM opera* /F
+timeout /T 30
+taskkill /T /IM opera* /F
+timeout /T 50
+taskkill /T /IM opera* /F
+timeout /T 20
+taskkill /T /IM opera* /F
+timeout /T 40
+goto :loop
