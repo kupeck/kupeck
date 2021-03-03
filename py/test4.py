c2e487409a11b0115f2aca0c481c40b205a65e39
kupeck <70723820+kupeck@users.noreply.github.com>
2021-03-03 18:50:48 +0100

    Create test4.py

---
+++ py/test4.py
@@ -0,0 +1,12 @@
+i = 1
+while i < 1001:
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(str(i))
+    i = i + 1
+print("done.")
